5c7fce3700ddb2b68f7062899f1b764d774dd4d7
TaufiqHA <htaufiq225@gmail.com>
2022-08-16 22:04:27 +0700

    function overriding

--- data/Manager.php
+++ data/Manager.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 
 class Manager
 {
 	var string $name;
 
-	function sayHello(string $name): void 
+	function sayHello(string $name): void
 	{
 		echo "Hi $name, my name is $this->name" . PHP_EOL;
 	}
@@ -12,5 +12,8 @@ class Manager
 
 class vicePresident extends Manager
 {
-
+	public function sayHello(string $name): void
+	{
+		echo "hi $name, my name is vp $this->name";
+	}
 }
